6d7ea05035ecb6ba7fc55e61925abc3a46998a2b
Amory Meltzer <Amorymeltzer@gmail.com>
2015-10-06 15:53:40 -0700

    Two ways to get sheet count, hash of sheet names

--- multipleWorksheets.pl
+++ multipleWorksheets.pl
@@ -8,3 +8,14 @@ use warnings;
 use diagnostics;
 
 use Spreadsheet::Read;
+
+
+my $book = ReadData ($ARGV[0]);
+print "$book->[0]{'sheets'}\n";
+print "$book->[1]{'label'}\n";
+print "$book->[2]{'label'}\n";
+print "$book->[3]{'label'}\n";
+
+my %sheets = %{$book->[0]{'sheet'}};
+my @keys = keys %sheets;
+print "@keys\n";
