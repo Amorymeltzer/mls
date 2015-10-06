0266fca96f4519a08d74118689a55bfb18a06643
Amory Meltzer <Amorymeltzer@gmail.com>
2015-10-06 15:02:10 -0700

    Begin perl to parse multiple worksheets
    Should probably just use this instead of xlscat eventually

---
+++ multipleWorksheets.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/env perl
+# multipleWorksheets.pl by Amory Meltzer
+# Prepare XLS/X files with multiple worksheets for xlscat
+# Probably better to just end run around xlscat in the long run, eh?
+
+use strict;
+use warnings;
+use diagnostics;
+
+use Spreadsheet::Read;
