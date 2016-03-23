e0527fddb5865ba0adac2cc000dd5353e3efbc6b
Amory Meltzer <Amorymeltzer@gmail.com>
2016-03-23 15:05:43 -0700

    Use maxdepth to only move csvs in this folder
    Hopefully not needed later, but for now not a bad idea

--- generate_data.sh
+++ generate_data.sh
@@ -44,7 +44,8 @@ else
 
     # Grab everything...
     #  FILES=$(find -E . -regex "./.*mls_.*xlsx?" | grep -v _site)
-    FILES=$(find -E . -regex "./.*[sfu][0-9][0-9].*csv" | grep -v _site)
+    #  FILES=$(find -E . -regex "./.*[sfu][0-9][0-9].*csv" | grep -v _site)
+    FILES=$(find -E . -maxdepth 1 -regex "./.*[sfu][0-9][0-9].*csv" | grep -v _site)
     echo $FILES
 
     # # Sort files chronologically
