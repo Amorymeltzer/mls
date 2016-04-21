d4092df0ded530ec0b30419256ed2e0eb198a34b
Amory Meltzer <Amorymeltzer@gmail.com>
2016-04-20 21:59:13 -0700

    Add in ISO and GPA, just 'cause
    I want them, this will help deal with order

--- makeMLSTable.pl
+++ makeMLSTable.pl
@@ -270,6 +270,8 @@ Player Minor league science, Major league style
   SAC Sacrifice flies
   AVG Batting average - H/AB
   OBP On-Base Percentage - (H+BB)/(AB+BB+SAC)
-  wOBA Weighted On-Base Average - Uses linear weights to calculate true value of each offensive action
   SLG Sugging - TB/AB
   OPS On-base percentage Plus Slugging - OBP+SLG
+  ISO Isolated Power - SLG-AVG
+  GPA GPA
+  wOBA Weighted On-Base Average - Uses linear weights to calculate true value of each offensive action
